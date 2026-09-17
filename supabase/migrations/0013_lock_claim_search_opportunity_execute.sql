f438fa984980c1ab2d36583df0653a5332d16f39
WealthGrowthCapital <wealthgrowthcapital@gmail.com>
2026-09-17 19:36:10 -0400

    security: make search opportunity claim invoker

--- supabase/migrations/0013_lock_claim_search_opportunity_execute.sql
+++ supabase/migrations/0013_lock_claim_search_opportunity_execute.sql
@@ -1,2 +1,3 @@
+alter function public.claim_next_search_opportunity() security invoker;
 revoke all on function public.claim_next_search_opportunity() from public;
 grant execute on function public.claim_next_search_opportunity() to authenticated;
